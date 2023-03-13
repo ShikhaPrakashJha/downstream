3d49396b956d00c93b563dec0a8b8ec7ac2d8ec0
ShikhaPrakashJha <shikha.prakash1994@gmail.com>
2023-03-13 15:33:33 +0530

    downstream commit

---
+++ src/test/java/downstream/practice/test20.java
@@ -0,0 +1,20 @@
+package downstream.practice;
+
+import org.testng.annotations.Test;
+
+public class test20 {
+
+	@Test
+	public void test31() {
+		System.out.println("test---31");
+	}
+	@Test
+	public void test32() {
+		System.out.println("test---32");
+	}
+	
+	@Test
+	public void test33() {
+		System.out.println("test---33");
+	}
+}
---
+++ src/test/java/downstream/practice/test21.java
@@ -0,0 +1,19 @@
+package downstream.practice;
+
+import org.testng.annotations.Test;
+
+public class test21 {
+	@Test
+	public void test21() {
+		System.out.println("test---21");
+	}
+	@Test
+	public void test22() {
+		System.out.println("test---22");
+	}
+	
+	@Test
+	public void test23() {
+		System.out.println("test---23");
+	}
+}
